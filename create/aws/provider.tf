2ce0df4ae3a11a99e247eed993dd3de4db96b976
Tommy Speigner <tspeigner@gmail.com>
2017-07-21 04:25:37 +0000

    initial commit

---
+++ create/aws/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  region                     = "us-west-2"
+  shared_credentials_file    = "/home/centos/.aws/credentials"
+}
---
+++ create/vmware/provider.tf
@@ -0,0 +1,5 @@
+# Configure the VMware vSphere Provider
+provider "vsphere" {
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}

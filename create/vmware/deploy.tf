5a3e3245afc04c3f049ef76838184829b82d8b39
Tommy Speigner <tspeigner@gmail.com>
2018-09-12 16:23:39 -0700

    Update deploy.tf

--- create/vmware/deploy.tf
+++ create/vmware/deploy.tf
@@ -42,8 +42,6 @@ provisioner "remote-exec" {
       "sudo touch /opt/puppetlabs/puppet/cache/state/agent_disabled.lock",
       "sudo bash -c \"curl -k https://master.inf.puppet.vm:8140/packages/current/install.bash | sudo bash\"",
       "sudo bash -c \"/opt/puppetlabs/bin/puppet agent -t --agent_disabled_lockfile /tmp/puppet_first_run.lock\"",
-      "sudo bash -c \"/opt/puppetlabs/bin/puppet agent -t --agent_disabled_lockfile /tmp/puppet_first_run.lock\"",
-      "sudo bash -c \"/opt/puppetlabs/bin/puppet agent -t --agent_disabled_lockfile /tmp/puppet_first_run.lock \"",
       "sudo rm /opt/puppetlabs/puppet/cache/state/agent_disabled.lock",
       "sudo bash -c \"/opt/puppetlabs/bin/puppet agent --enable\"",
       "sudo bash -c \"/opt/puppetlabs/bin/puppet agent -t\"",
